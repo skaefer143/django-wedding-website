d418e26e7c742677bc07fe215effe869e24745d4
Storm Kaefer <stormkaefer@gmail.com>
2019-07-21 02:55:25 -0600

    wow it's way too late for me

--- wedding/views.py
+++ wedding/views.py
@@ -4,9 +4,8 @@ from guests.save_the_date import SAVE_THE_DATE_CONTEXT_MAP
 
 
 def home(request):
-    if settings.DEBUG:
-        return render(request, 'wip.html')
-    return render(request, 'home.html', context={
-        'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
-        'support_email': settings.DEFAULT_WEDDING_REPLY_EMAIL,
-    })
+    return render(request, 'wip.html')
+    # return render(request, 'home.html', context={
+    #     'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
+    #     'support_email': settings.DEFAULT_WEDDING_REPLY_EMAIL,
+    # })
